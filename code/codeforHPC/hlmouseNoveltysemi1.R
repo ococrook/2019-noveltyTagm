3f6fffc0a9e03b334c397bf182ee7496af59085f
Oliver M. Crook <ococrook@gmail.com>
2020-08-10 21:45:57 +0100

    additional analysis and update paper

---
+++ code/codeforHPC/hlmouseNoveltysemi1.R
@@ -0,0 +1,33 @@
+## mouse HPC script for sensitivity analysis
+
+require(MSnbase)
+require(pRoloc)
+require(pRolocdata)
+require(mcclust)
+require(pheatmap)
+
+source("machinelearning-framework-tagm-Novelty.R")
+source("machinelearning-functions-tagm-Novelty.R")
+
+data("hyperLOPIT2015")
+fData(hyperLOPIT2015)$markers[fData(hyperLOPIT2015)$markers %in% c("40S Ribosome","60S Ribosome",
+                                                                   "Nucleus - Chromatin", "Nucleus - Non-chromatin")] <- "unknown"
+
+
+## hide additional annotations randomly
+sum(fData(hyperLOPIT2015)$markers != "unknown") #707 markers
+toMask <- sample(which(fData(hyperLOPIT2015)$markers != "unknown"), 70)
+fData(hyperLOPIT2015)$markers[toMask] <- "unknown"
+
+
+hlmouseTagmNoveltyparams_semi01 <- tagmMcmcTrain_Nov(object = hyperLOPIT2015,
+                                                     fcol = "markers",
+                                                     numIter = 15000,
+                                                     thin = 10,
+                                                     burnin = 5000,
+                                                     numChains = 6,
+                                                     beta0 = 1,
+                                                     K_bar = 10)
+
+
+save(hlmouseTagmNoveltyparams_semi01, file = "/home/omc25/rds/hpc-work/hlmouseTagmNoveltyparams_semi01.rda")
---
+++ code/codeforHPC/hlmouseNoveltysens1.R
@@ -0,0 +1,28 @@
+## mouse HPC script for sensitivity analysis
+
+require(MSnbase)
+require(pRoloc)
+require(pRolocdata)
+require(mcclust)
+require(pheatmap)
+
+source("machinelearning-framework-tagm-Novelty.R")
+source("machinelearning-functions-tagm-Novelty.R")
+
+data("hyperLOPIT2015")
+fData(hyperLOPIT2015)$markers[fData(hyperLOPIT2015)$markers %in% c("40S Ribosome","60S Ribosome",
+                                                                   "Nucleus - Chromatin", "Nucleus - Non-chromatin")] <- "unknown"
+
+
+
+hlmouseTagmNoveltyparams_beta01 <- tagmMcmcTrain_Nov(object = hyperLOPIT2015,
+                                                     fcol = "markers",
+                                                     numIter = 15000,
+                                                     thin = 10,
+                                                     burnin = 5000,
+                                                     numChains = 6,
+                                                     beta0 = 0.1,
+                                                     K_bar = 10)
+
+
+save(hlmouseTagmNoveltyparams_beta01, file = "/home/omc25/rds/hpc-work/hlmouseTagmNoveltyparams_beta01.rda")
